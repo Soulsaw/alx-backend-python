cb46b390f14018114be42cb94c4a018e1021a893
Soulsaw <ssawadogo292@gmail.com>
2024-03-15 03:36:31 +0000

    fix all

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
+from typing import Union
 """Doc module"""
+
+
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+    """This function return the sum of a list of int and float"""
+    return sum(mxd_lst)
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
+from typing import Callable
 """Doc module"""
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """This function"""
+    def fun(x: float) -> float:
+        """A callable function"""
+        return x * multiplier
+    return fun
