efb5e6f19fa2a01724148dcaf9e464bb031e0d76
Soulsaw <ssawadogo292@gmail.com>
2024-03-16 23:39:28 +0000

    fix all

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,10 +2,11 @@
 """Doc module"""
 from typing import Mapping, Union, Any, TypeVar
 """Import of all modules"""
-
-
 T = TypeVar("T")
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) \
+                    -> Union[Any, T]:
     """This function meet with the requirement"""
     if key in dct:
         return dct[key]
