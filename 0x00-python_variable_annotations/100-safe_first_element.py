60f68c57160ac8e1da9ee16e85a7e1fd3926c5ac
Soulsaw <ssawadogo292@gmail.com>
2024-03-16 23:25:08 +0000

    fix advanced task

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 """Doc module"""
+from typing import Sequence, Any, Union
+"""Import all the need modules"""
+
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """This function return the rigth"""
+    if lst:
+        return lst[0]
+    else:
+        return None
