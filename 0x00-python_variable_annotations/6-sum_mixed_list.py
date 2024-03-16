f639c87bf185256cd4457369678ccd873376b868
Soulsaw <ssawadogo292@gmail.com>
2024-03-16 23:07:58 +0000

    fix task 6 bug

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Import of all modules"""
-from typing import Union
+from typing import List, Union
 """Doc module"""
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """This function return the sum of a list of int and float"""
     return sum(mxd_lst)
