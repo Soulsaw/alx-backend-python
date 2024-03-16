f7aa8d969ffe179a277f0a3310e8b9a6a51e22bd
Soulsaw <ssawadogo292@gmail.com>
2024-03-16 23:26:35 +0000

    fix all

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,6 +3,7 @@
 from typing import Sequence, Any, Union
 """Import all the need modules"""
 
+
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """This function return the rigth"""
