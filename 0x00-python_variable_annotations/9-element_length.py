c34459fa4b80c986ee91da3b4029941a3e5796ce
Soulsaw <ssawadogo292@gmail.com>
2024-03-16 23:20:32 +0000

    fix task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 """Doc module"""
+from typing import Iterable, Sequence, List, Tuple
+"""Import of all modules"""
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """This function return the length of an element"""
+    return [(i, len(i)) for i in lst]
