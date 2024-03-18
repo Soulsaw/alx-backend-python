c7d98dc991d29bf9063cfb0f7766f1d1e5e5983f
Soulsaw <ssawadogo292@gmail.com>
2024-03-18 23:11:09 +0000

    fix all

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """Module doc"""
 import asyncio
+from typing import List
 """Import of the random module"""
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Appel async methode"""
     wait_random = __import__('0-basic_async_syntax').wait_random
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
+    delays.sort()
     return delays
