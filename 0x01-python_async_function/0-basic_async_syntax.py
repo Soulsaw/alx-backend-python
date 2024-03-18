e20e19184d5d2f8d5dc1c7f2f78bce443f606970
Soulsaw <ssawadogo292@gmail.com>
2024-03-18 22:43:46 +0000

    first commit

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module doc"""
+import random
+import asyncio
+"""Import of the random module"""
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """This function permit to wait for"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
