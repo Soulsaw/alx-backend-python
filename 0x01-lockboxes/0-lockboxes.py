d1411148b3df420f7d18c594ad6819acfd11d1e6
Soulsaw <ssawadogo292@gmail.com>
2024-03-18 01:24:28 +0000

    fix the lockboxes projet

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Doc of the module"""
+
+
+def canUnlockAll(boxes):
+    """This function if all the box can be open"""
+    if not boxes or len(boxes) == 0:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+
+    queue = [0]  # Start with the first box
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
